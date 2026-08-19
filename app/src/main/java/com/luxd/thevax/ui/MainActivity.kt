0ebea480de48e90317ad31f8feaa67068bcef1b9
LuXD-05 <alessio.luciano@megablu.it>
2026-08-19 20:31:18 +0200

    - Made MainActivity.kt just a session checker (if logged --> SplashScreen, else LoginActivity) - Updated SplashScreen.kt as a view controller that allows switching through a bottom navigation bar (bottom_menu.xml)

--- app/src/main/java/com/luxd/thevax/ui/MainActivity.kt
+++ app/src/main/java/com/luxd/thevax/ui/MainActivity.kt
@@ -1,31 +1,19 @@
-package com.luxd.thevax
+package com.luxd.thevax.ui
 
 import android.content.Intent
-import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.luxd.thevax.services.SessionService
-import com.luxd.thevax.ui.auth.LoginActivity
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: android.os.Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // If not logged, go directly to login
-        if (!SessionService.getInstance(this).isLoggedIn()) {
+        // If not logged in, go directly to login
+        if (SessionService.getInstance(this).isLoggedIn()) {
+            startActivity(Intent(this, SplashScreen::class.java))
+        } else {
             startActivity(Intent(this, LoginActivity::class.java))
-            finish()
-            return
         }
-
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        finish()
     }
 }
---
+++ app/src/main/java/com/luxd/thevax/ui/SplashScreen.kt
@@ -0,0 +1,48 @@
+package com.luxd.thevax.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.luxd.thevax.R
+import androidx.activity.enableEdgeToEdge
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.luxd.thevax.databinding.ActivitySplashScreenBinding
+import androidx.fragment.app.FragmentManager
+
+@Suppress("CustomSplashScreen")
+class SplashScreen : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashScreenBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashScreenBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+        enableEdgeToEdge()
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        // 1st launch --> auto-switch to Home
+        if (savedInstanceState == null)
+            supportFragmentManager.beginTransaction().replace(R.id.tab_container, HomeFragment()).commit()
+
+        // Set bottom bar view switching listener
+        binding.bottomMenu.setOnItemSelectedListener { tab ->
+
+            // PopBack stack reset (resets view stack when switching tabs)
+            supportFragmentManager.popBackStack(null, FragmentManager.POP_BACK_STACK_INCLUSIVE)
+
+            when (tab.itemId) {
+                R.id.home_tab -> supportFragmentManager.beginTransaction().replace(R.id.tab_container, HomeFragment()).commit()
+                R.id.calendar_tab -> supportFragmentManager.beginTransaction().replace(R.id.tab_container, CalendarFragment()).commit()
+                R.id.profile_tab -> supportFragmentManager.beginTransaction().replace(R.id.tab_container, ProfileFragment()).commit()
+            }
+            true
+        }
+    }
+}
